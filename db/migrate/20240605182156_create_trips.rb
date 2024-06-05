fa6417b78156b2c1bd38e87e96f7d1590f07d5a2
Edyta Barańska <edytacr@gmail.com>
2024-06-05 20:33:07 +0200

    trips migration

---
+++ db/migrate/20240605182156_create_trips.rb
@@ -0,0 +1,12 @@
+class CreateTrips < ActiveRecord::Migration[7.0]
+  def change
+    create_table :trips do |t|
+      t.string :start_address, null: false
+      t.string :destination_address, null: false
+      t.decimal :price, precision: 8, scale: 2, null: false
+      t.date :date, null: false
+
+      t.timestamps
+    end
+  end
+end
