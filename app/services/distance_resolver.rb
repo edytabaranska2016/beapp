5c02c46b229dd79da1e345163e265f73494224a0
Edyta Barańska <edytacr@gmail.com>
2024-06-10 16:53:06 +0200

    change

--- app/services/distance_resolver.rb
+++ app/services/distance_resolver.rb
@@ -6,7 +6,7 @@ class DistanceResolver
   end
 
   def perform
-    Geocoder::Calculations.distance_between(start_coordinates, destination_coordinates)
+    Geocoder::Calculations.distance_between(start_coordinates(trip), destination_coordinates(trip))
   end
 
   private
@@ -15,12 +15,11 @@ class DistanceResolver
 
   def start_coordinates(trip)
     start_location = Location.find_by(address: trip.start_address)
-    [start_location.latitude,start_location.longitude]
+    start_location.nil? ? 0: [start_location.latitude,start_location.longitude]
   end
 
   def destination_coordinates(trip)
     destination_location = Location.find_by(address: trip.destination_address)
-    [destination_location.latitude,destination_location.longitude]
+    destination_location.nil? ? 0 : [destination_location.latitude,destination_location.longitude]
   end
-
 end
