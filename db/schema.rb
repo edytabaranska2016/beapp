5674b990848143a66501c679db5c80e360d545f0
Edyta Barańska <edytacr@gmail.com>
2024-06-12 20:33:46 +0200

    add distance to trips

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_10_112113) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_12_183300) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,6 +33,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_10_112113) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+    t.float "distance"
   end
 
 end
